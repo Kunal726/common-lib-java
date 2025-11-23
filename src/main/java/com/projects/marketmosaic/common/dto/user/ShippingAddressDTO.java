fd4ed3522c5f377d840dbadb50270b672682ec0b
Kunal <kunalkulthe01@gmail.com>
2025-11-23 18:17:45 +0530

    added ShippingAddressDTO

---
+++ src/main/java/com/projects/marketmosaic/common/dto/user/ShippingAddressDTO.java
@@ -0,0 +1,19 @@
+package com.projects.marketmosaic.common.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ShippingAddressDTO {
+    private Long id;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String phoneNumber;
+    private String addressType;
+    private boolean isDefault;
+}
